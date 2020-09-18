c37a4041a1798f660e736a58cfa0fe873a0ee97a
unknown <chucky-qiu@lambdastudents.com>
2020-09-18 19:25:20 -0400

    setting up basic yup validation

--- react-1/src/Components/Form.js
+++ react-1/src/Components/Form.js
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 import {Link} from "react-router-dom";
 import Login from "./Login";
 import styled from "styled-components";
+import * as yup from "yup";
 
 export default function Form() {
     //state
@@ -11,6 +12,42 @@ export default function Form() {
         // email: "",
         sellers: false
     });
+
+    //formschema
+    const formSchema = yup.object().shape({
+        username: yup
+        .string()
+        .min(2, "Atleast 2 characters")
+        .required(),
+        
+        password: yup
+        .string()
+        .min(6, "Atleast 6 characters")
+        .required(),
+
+        sellers: yup
+        .boolean()
+    })
+
+    //errorState
+    const [errors, setErrors] = useState({
+        username: "",
+        password: "",
+    });
+
+    //validate
+    const validate = event => {
+    yup
+    .reach(formSchema, event.target.name)
+    .validate(event.target.value)
+    .then(valid => {
+        setErrors({...errors, [event.target.name]: ""});
+    })
+    .catch(err => {
+        setErrors({...errors, [event.target.name]: err.errors[0]});
+    });
+    };
+    
     //onChange
     const inputChange = event => {
         event.persist();
