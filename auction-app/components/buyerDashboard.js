0822710e7abdd523adb3a64eb151be1356fedabb
unknown <Katherine2bk@gmail.com>
2020-09-19 09:54:16 -0700

    set up buyer dashboard, savedAuction component... working on sellerDashboard next

--- auction-app/components/buyerDashboard.js
+++ auction-app/components/buyerDashboard.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 import Auctions from "./Auctions";
 
-function BuyerDashboard() {
+// want to set up a dashboard where buyer can place bids
+// and save those auctions he placed a bid on
+
+const BuyerDashboard = () => {
+    const [watchingAuction, setWatchingAuctions] = useState([])
+    const [auctions, setAuctions] = useState({user_id: "", id: ""});
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get("waiting for url")
+        .then(response => {
+            console.log("axios from buyer dashboard", response)
+            setWatchingAuctions(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    })
+
     return (
-        <div className="background-box">
-            <div className="box">
-            <Auctions />
-            </div>
+        <div>
+        <h2>Your bids:</h2>
+        <div className="dashboard">
+        <SavedAuctions list={watchedAuctions}/>
+        </div>
+        {
+            auctions.map(auction => {
+                <Auctions auction={auction} />
+            })
+        }
         </div>
     )
 }
